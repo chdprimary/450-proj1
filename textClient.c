93a17b81430649f20e0ae19bd0e6fa587d16fd56
chdprimary <root@wintermute-com.(none)>
2013-12-23 00:29:26 -0600

    Modified message input to conform to instructions. Added printable usage.

--- textClient.c
+++ textClient.c
@@ -1,3 +1,8 @@
+/*
+	TODO:
+	* Encapsulate everything into functions
+*/
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -8,6 +13,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+
+
 
 int main(int argc, char *argv[])
 {
@@ -20,14 +27,41 @@ int main(int argc, char *argv[])
 	memset(sendBuff,'0',sizeof(sendBuff));
 
 
-	/*      we take the first cmdline arg as the message to be passed       */
-	if (argc!=2)
+	/*	print usage for --help flag OR invalid number of args	*/
+	if ( ((argv[0] == "./textClient" || argv[0] == "textClient") && argv[1] == "--help") || argc != 1 )
 	{
-		printf("You must enter a message as an argument!\n");
-		return 0;
+		printf("\nUsage: [cat FILE_IN.TXT |] ./textClient\n");
+		printf("\tTakes a message from a file or from stdin and transmits it to a specified port.\n\n");
+		return 1;
 	}
-	else
-		strcpy(sendBuff,argv[1]);
+
+
+	/*      We take the first cmdline arg as the message to be passed     */
+	if (isatty(STDIN_FILENO))
+		printf("\nPlease enter your message:\n");
+	char ch = getchar();
+	int index = 0;
+	//can type EOF and \0 ?
+	while(ch != -1 && ch != 0)
+	{
+		if (ch == '\n')
+		{
+			if ( (ch = getchar()) == '\n')
+			{
+				ch = -1;
+				break;
+			}
+			else
+			{
+				ungetc(ch,stdin);
+				ch = '\n';
+			}
+		}
+		sendBuff[index] = ch;
+		++index;
+		ch = getchar();
+	}
+	sendBuff[index] = '\0';
 
 
 	/*	set sockfd as a socket with TCP over internet	*/
@@ -46,7 +80,8 @@ int main(int argc, char *argv[])
 	serv_addr.sin_addr.s_addr 	= inet_addr("127.0.0.1");
 
 
-	/*	perform an ACTIVE connection over the socket with the machine at serv_addr	*/
+	/*	perform an ACTIVE connection over the socket with the machine
+		at serv_addr	*/
 	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
 	{
 		printf("\nError: Connect Failed\n");
@@ -57,7 +92,7 @@ int main(int argc, char *argv[])
 	/*      write buffer into connection
                 close connection		*/
 	write(sockfd,sendBuff,strlen(sendBuff));
-	sleep(1);
+//	sleep(1);
 	close(sockfd);
 
 	/*	ends program	*/
