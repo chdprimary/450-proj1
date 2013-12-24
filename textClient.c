0d1782088c2158689ce8c4eb37f3bc526c1ddd93
chdprimary <root@wintermute-com.(none)>
2013-12-23 20:18:06 -0600

    Completely modular textClient. Lots of stdout feedback, so more user friendly. Error handling. Be on the lookout for errors and bugs with this guy -- it hasn't been stress tested.

--- textClient.c
+++ textClient.c
@@ -1,7 +1,11 @@
-/*
-	TODO:
-	* Encapsulate everything into functions
-*/
+/****************************************************************
+ * Christian Dean                      |\      _,,,---,,_       *
+ * Due: 12/15/14                 ZZZzz /,`.-'`'    -.  ;-;;,_   *
+ * textClient.c                       |,4-  ) )-,_. ,\ (  `'-'  *
+ *                                   '---''(_/--'  `-'\_)       *
+ *                                                              *
+ * A program that
+ ***************************************************************/
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -14,31 +18,72 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-void checkargs(int,char**);
+void check_args(int,char**);
+char *get_message();
+int connect_serv(const char*,int,int,struct sockaddr_in);
+void send_message(int,char*);
+void disconnect_serv(int);
 
 int main(int argc, char *argv[])
 {
 	int sockfd = 0;
-	char sendBuff[1025];
 	struct sockaddr_in serv_addr;
+	char *sendBuff = malloc(sizeof(char) * 1025);
 
-	checkargs(argc,argv);
 
+	/*	Check args to ensure textClient has been called correctly.
+		Then store hostname and port number specified by args 	*/
+	check_args(argc,argv);
 	const char *HOST = argv[1];
 	const int PORT = atoi(argv[2]);
 
 
-	/*	Zero out message buffer	to be sent	*/
+	/*	Zero out message buffer	to be sent.
+		Then assign value of message from stdin or file	*/
 	memset(sendBuff,'0',sizeof(sendBuff));
+	sendBuff = get_message();
 
 
-	/*	Get message from stdin or file char by char. Store in sendBuff. Append a '\0'.	*/
+	/*	Create connection to server.
+		Send message in sendBuff	*/
+	sockfd = connect_serv(HOST,PORT,sockfd,serv_addr);
+	send_message(sockfd,sendBuff);
+
+
+	/*	Clean up and exit	*/
+	free(sendBuff);
+	disconnect_serv(sockfd);
+	printf("\nexiting textClient.\n\n");
+	return 0;
+}
+
+void check_args(int argc, char *argv[])
+{
+	printf("\ncheck_args(): checking program arguments...");
+	/*	Print usage for --help flag OR invalid number of args	*/
+	if ( ((argv[0] == "./textClient" || argv[0] == "textClient") && argv[1] == "--help") || argc != 3 )
+	{
+		printf("\nUsage: [cat FILE_IN.TXT |] ./textClient HOST PORT_NUM\n");
+		printf("\tTakes a text message from stdin or from a pipe, and transmits it to HOST:PORT_NUM.\n\n");
+		exit(1);
+	}
+	else
+		printf("\n\ttextClient started succesfully.");
+}
+
+char *get_message()
+{
+	printf("\nget_message(): getting text message...");
+	char *sendBuff = malloc(sizeof(char) * 1025);
+
+	/*	Get message from stdin or file char by char.
+		Store in sendBuff.
+		Append a '\0'			*/
 	if (isatty(STDIN_FILENO))
-		printf("\nPlease enter your message:\n");
+		printf("\n\tPlease type a message to be sent (press ENTER twice to finish):\n\nMESSAGE\n------------------------------\n");
 	char ch = getchar();
 	int index = 0;
-	//can type EOF and \0 ?
-	while(ch != -1 && ch != 0)
+	while(ch != EOF && ch != '\0')
 	{
 		if (ch == '\n')
 		{
@@ -58,50 +103,67 @@ int main(int argc, char *argv[])
 		ch = getchar();
 	}
 	sendBuff[index] = '\0';
+	/*	deletes newline control sequence from stdout - for readability
+		--------------------------------------------	*/
+	printf("\033[A");
+	printf("\033[K");
+	printf("\033[A");
+	/*	-------------------------------------------	*/
 
+	printf("\n------------------------------\n");
+	printf("\n\ttext message ready to send.");
+	return sendBuff;
+}
 
-	/*	set sockfd as a socket with TCP over internet	*/
+int connect_serv(const char *HOST,int PORT,int sockfd,struct sockaddr_in serv_addr)
+{
+	printf("\nconnect_serv(): getting socket, establishing connection to server...");
+	/*	Set sockfd as a socket with TCP over internet	*/
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("Could not create socket \n");
-		return 1;
+		printf("\n\tError: Could not create socket.");
+		exit(1);
 	}
 
 
 	/*	define the domain used by serv_addr
-	        use port 5000
-		set internet address as localhost	*/
+		use port PORT
+		set destination address as HOST		*/
 	serv_addr.sin_family 		= AF_INET;
 	serv_addr.sin_port 		= htons(PORT);
 	serv_addr.sin_addr.s_addr 	= inet_addr(HOST);
 
 
-	/*	perform an ACTIVE connection over the socket with the machine
-		at serv_addr	*/
-	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+	/*	perform an ACTIVE connection over the socket
+		with the location of serv_addr			*/
+	if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0 )
 	{
-		printf("\nError: Connect Failed\n");
-		return 1;
-	}
-
-
-	/*      write buffer into connection
-                close connection		*/
-	write(sockfd,sendBuff,strlen(sendBuff));
-//	sleep(1);
-	close(sockfd);
-
-	/*	ends program	*/
-	return 0;
-}
-
-void checkargs(int argc, char *argv[])
-{
-	/*	Print usage for --help flag OR invalid number of args	*/
-	if ( ((argv[0] == "./textClient" || argv[0] == "textClient") && argv[1] == "--help") || argc != 3 )
-	{
-		printf("\nUsage: [cat FILE_IN.TXT |] ./textClient HOST PORT_NUM\n");
-		printf("\tTakes a text message from stdin or from a pipe, and transmits it to HOST:PORT_NUM.\n\n");
+		printf("\n\tError: Connection to server failed.");
 		exit(1);
 	}
+	printf("\n\tconnection to %s : %d successful.",HOST,PORT);
+	return sockfd;
+}
+
+void send_message(int sockfd, char *sendBuff)
+{
+	printf("\nsend_message(): Attempting to send message...");
+	/*	write buffer into connection	*/
+	if( write(sockfd,sendBuff,strlen(sendBuff)) < 0)
+	{
+		printf("\n\tError: Transmission of message failed -- sendBuff: %s",sendBuff);
+		exit(1);
+	}
+	printf("\n\tMessage sent successfully.");
+}
+
+void disconnect_serv(int sockfd)
+{
+	printf("\ndisconnect_serv(): Attempting to close connection to server...");
+	/*	close connection	*/
+	if( close(sockfd) < 0 )
+	{
+		printf("\n\tError: Could not close socket file descriptor -- sockfd: %d.",sockfd);
+	}
+	printf("\n\tConnection closed successfully.");
 }
