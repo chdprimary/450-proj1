946c8c4376b3133132bab9fa2ce15d9ee086b422
chdprimary <root@wintermute-com.(none)>
2013-12-24 22:38:37 -0600

    Fixed textClient.c & textServer.c. Last commit transmitted broken messages.

--- textClient.c
+++ textClient.c
@@ -53,34 +53,42 @@ int main(int argc, char *argv[])
 	/*	Clean up and exit	*/
 	free(sendBuff);
 	disconnect_serv(sockfd);
-	printf("\nexiting textClient.\n\n");
+	printf("\nexiting textClient.\n\n"); usleep(500 * 1000);
 	return 0;
 }
 
 void check_args(int argc, char *argv[])
 {
-	printf("\ncheck_args(): checking program arguments...");
+	printf("\ncheck_args(): checking program arguments..."); usleep(500 * 1000);
+	if(strcmp(argv[1],"--lh") == 0)
+	{
+//		strcpy(argv[1],"127.0.0.1");	//this doesn't work for some reason
+		argv[1] = "127.0.0.1";
+	}
 	/*	Print usage for --help flag OR invalid number of args	*/
 	if ( ((argv[0] == "./textClient" || argv[0] == "textClient") && argv[1] == "--help") || argc != 3 )
 	{
 		printf("\nUsage: [cat FILE_IN.TXT |] ./textClient HOST PORT_NUM\n");
+		printf("\n\tFlags: --lh: sets HOST as '127.0.0.1' if specified in place of HOST argument.\n");
 		printf("\tTakes a text message from stdin or from a pipe, and transmits it to HOST:PORT_NUM.\n\n");
 		exit(1);
 	}
 	else
-		printf("\n\ttextClient started succesfully.");
+	{
+		printf("\n\ttextClient started successfully.");usleep(500 * 1000);
+	}
 }
 
 char *get_message()
 {
-	printf("\nget_message(): getting text message...");
+	printf("\nget_message(): getting text message..."); usleep(500 * 1000);
 	char *sendBuff = malloc(sizeof(char) * 1025);
 
 	/*	Get message from stdin or file char by char.
 		Store in sendBuff.
 		Append a '\0'			*/
 	if (isatty(STDIN_FILENO))
-		printf("\n\tPlease type a message to be sent (press ENTER twice to finish):\n\nMESSAGE\n------------------------------\n");
+		printf("\n\tPlease type a message to be sent (press ENTER twice to finish):\n\nMESSAGE\n------------------------------\n"); usleep(500 * 1000);
 	char ch = getchar();
 	int index = 0;
 	while(ch != EOF && ch != '\0')
@@ -103,25 +111,29 @@ char *get_message()
 		ch = getchar();
 	}
 	sendBuff[index] = '\0';
-	/*	deletes newline control sequence from stdout - for readability
-		--------------------------------------------	*/
-	printf("\033[A");
-	printf("\033[K");
-	printf("\033[A");
-	/*	-------------------------------------------	*/
+	if (isatty(STDIN_FILENO))
+	{
+		/*	deletes newline control sequence from stdout - for readability
+			--------------------------------------------	*/
+		printf("\033[A");
+		printf("\033[K");
+		printf("\033[A");
+		/*	-------------------------------------------	*/
 
-	printf("\n------------------------------\n");
-	printf("\n\ttext message ready to send.");
+		printf("\n------------------------------\n");
+	}
+	printf("\n\ttext message ready to send."); usleep(500 * 1000);
 	return sendBuff;
 }
 
 int connect_serv(const char *HOST,int PORT,int sockfd,struct sockaddr_in serv_addr)
 {
-	printf("\nconnect_serv(): getting socket, establishing connection to server...");
+	printf("\nconnect_serv(): getting socket, establishing connection to server..."); usleep(500 * 1000);
+
 	/*	Set sockfd as a socket with TCP over internet	*/
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n\tError: Could not create socket.");
+		printf("\n\tError: Could not create socket."); usleep(500 * 1000);
 		exit(1);
 	}
 
@@ -138,32 +150,36 @@ int connect_serv(const char *HOST,int PORT,int sockfd,struct sockaddr_in serv_ad
 		with the location of serv_addr			*/
 	if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0 )
 	{
-		printf("\n\tError: Connection to server failed.");
+		printf("\n\tError: Connection to server failed.\n"); usleep(500 * 1000);
 		exit(1);
 	}
-	printf("\n\tconnection to %s : %d successful.",HOST,PORT);
+	printf("\n\tconnection to %s : %d successful.",HOST,PORT); usleep(500 * 1000);
 	return sockfd;
 }
 
 void send_message(int sockfd, char *sendBuff)
 {
-	printf("\nsend_message(): Attempting to send message...");
+	printf("\nsend_message(): Attempting to send message of size %d...",(int)strlen(sendBuff));
 	/*	write buffer into connection	*/
 	if( write(sockfd,sendBuff,strlen(sendBuff)) < 0)
 	{
-		printf("\n\tError: Transmission of message failed -- sendBuff: %s",sendBuff);
+		printf("\n\tError: Transmission of message failed -- sendBuff: %s",sendBuff); usleep(500 * 1000);
 		exit(1);
 	}
-	printf("\n\tMessage sent successfully.");
+	printf("\n\tMessage sent successfully."); usleep(500 * 1000);
+
 }
 
 void disconnect_serv(int sockfd)
 {
-	printf("\ndisconnect_serv(): Attempting to close connection to server...");
+	printf("\ndisconnect_serv(): Attempting to close connection to server..."); usleep(500 * 1000);
 	/*	close connection	*/
 	if( close(sockfd) < 0 )
 	{
-		printf("\n\tError: Could not close socket file descriptor -- sockfd: %d.",sockfd);
+		printf("\n\tError: Could not close socket file descriptor -- sockfd: %d.",sockfd); usleep(500 * 1000);
+
 	}
-	printf("\n\tConnection closed successfully.");
+	printf("\n\tConnection closed successfully."); usleep(500 * 1000);
+
 }
+
--- textServer.c
+++ textServer.c
@@ -15,33 +15,52 @@
 #include <string.h>
 #include <sys/types.h>
 
-void checkargs(int argc, char *argv[]);
+void check_args(int,char**);
+int setup(int,int,struct sockaddr_in);
+void start_listening(int);
+void print_messages(int);
 
 int main(int argc, char *argv[])
 {
 	int i;
-	int n;
-	int listenfd=0, connfd=0;
-
+	int listenfd = 0;
 	struct sockaddr_in serv_addr;
-	char recvBuff[1024];
-	int numrv;
 
-	checkargs(argc,argv);
+	/*	zero out serv_addr struct	*/
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
+	check_args(argc,argv);
 	const int PORT = atoi(argv[1]);
 
+	listenfd = setup(PORT,listenfd,serv_addr);
+
+	start_listening(listenfd);
+
+	print_messages(listenfd);
+
+	printf("\nNow exiting.");
+	return 0;
+}
+
+void check_args(int argc, char *argv[])
+{
+        printf("\ncheck_args(): checking program arguments..."); usleep(500 * 1000);
+        /*      Print usage for --help flag OR invalid number of args   */
+        if ( ((argv[0] == "./textServer" || argv[0] == "textServer") && argv[1] == "--help") || argc != 2 )
+        {
+                printf("\nUsage: ./textServer PORT_NUM\n");
+                printf("\tListens for text messages on PORT_NUM and displays them on stdout.\n\n");
+                exit(1);
+        }
+	printf("\n\ttextServer started successfully.");
+}
+
+int setup(int PORT,int listenfd,struct sockaddr_in serv_addr)
+{
+        printf("\nsetup(): Getting socket, binding socket to server port..."); usleep(500 * 1000);
 	/*	set listenfd as a socket with TCP over internet
 		print success message				*/
 	listenfd = socket(AF_INET,SOCK_STREAM,0);
-	printf("socket retrieve success\n");
-
-
-	/*	zero out serv_addr struct
-		zero out recvBuff		*/
-	memset(&serv_addr, '0', sizeof(serv_addr));
-	memset(recvBuff, '0', sizeof(recvBuff));
-
 
 	/*	define the domain used by serv_addr
 		permit any IP address with INADDR_ANY
@@ -50,50 +69,54 @@ int main(int argc, char *argv[])
 	serv_addr.sin_addr.s_addr 	= htonl(INADDR_ANY);
 	serv_addr.sin_port 		= htons(PORT);
 
-
 	/*	bind socket to serv_addr	*/
 	bind(listenfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
 
+	printf("\n\tSetup successful."); usleep(500 * 1000);
+	return listenfd;
+}
 
+void start_listening(int listenfd)
+{
+	printf("\nstart_listening(): Attempting to begin listening for connections...");
 	/*	start listening with 10 maximum possible simultaneous requests	*/
 	if(listen(listenfd,10) == -1) { printf("Failed to listen\n"); }
+	printf("\n\tNow listening for connections.");
+}
 
+void print_messages(int listenfd)
+{
+	printf("\nprint_messages(): Now accepting incoming text messages...\n");
+
+	int n;
+	int connfd = 0;
+	char recvBuff[1024];
+
+	memset(recvBuff, '0', sizeof(recvBuff));
 
 	/*	accept incoming connection from client
                 reads the message, places in recvBuff, appends a zero   */
 	while(1)
 	{
 		connfd = accept(listenfd, (struct sockaddr*)NULL,NULL);
-
+		printf("\nMessage\n----------------------\n");
 		while((n = read(connfd, recvBuff, sizeof(recvBuff)-1)) > 0)
 		{
 			recvBuff[n] = 0;
                 	if(fputs(recvBuff,stdout) == EOF)
                 	        printf("\nError: Fputs error");
-        	        printf("\n");
+        	        printf("\n----------------------\n");
 		}
 
 
-	    /*      if read() returned a -1, relay the error to user        */
+		/*      if read() returned a -1, relay the error to user        */
 		if(n < 0)
 		{
-			printf("\nError in socket\n");
+			printf("\n\tError in socket\n");
 			exit(0);
 		}
 
 		sleep(1);
 	}
-
-	return 0;
 }
 
-void checkargs(int argc, char *argv[])
-{
-        /*      Print usage for --help flag OR invalid number of args   */
-        if ( ((argv[0] == "./textServer" || argv[0] == "textServer") && argv[1] == "--help") || argc != 2 )
-        {
-                printf("\nUsage: ./textServer PORT_NUM\n");
-                printf("\tListens for text messages on PORT_NUM and displays them on stdout.\n\n");
-                exit(1);
-        }
-}
